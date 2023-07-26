170ff628499adbd3819ccf8252056fd65b88caef
unknown <ashishkumarnath9@gmail.com>
2023-07-26 20:37:02 +0530

    day-2 next expense app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Delete, Get, Post, Put,Param } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put,Param,Body, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Data, data } from './data';
+import { Data, ReportType, data } from './data';
+
 
 @Controller('report/:type')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('')
-  getAllIncomeReports(@Param('type') type:string): Data {
-    console.log({type})
-    return data;
+  getAllIncomeReports(@Param('type') type:ReportType) {
+
+    // let reportType = type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    
+    return this.appService.getAllIncomeReports(type)
   }
 
   @Get('hello')
@@ -17,22 +20,80 @@ export class AppController {
     return {};
   }
   @Get(':id')
-  getIncomeReportById():{} {
-    return {};
+  getIncomeReportById(@Param('type' )type:ReportType, @Param('id') id:string):{} {
+    // let reportType = type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    // let response = data.report.filter((data)=>data.type===reportType).find((data)=>data.id===id)
+    // console.log({response})
+    return this.appService.getIncomeReportById(type,id)
   }
 
   @Post()
-  createReport(){
-    return "created"
+  createReport( @Body() body:{amount:number,source:string}, @Param('type') type:ReportType){
+    // let newReport = {
+    //   id:uuid(),
+    //   source:source,
+    //   amount:amount,
+    //   createdAt:new Date(),
+    //   updatedAt:new Date(),
+    //   type:type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    // }
+
+    // data.report.push(newReport)
+
+    // console.log()
+    return this.appService.createReport(body,type)
   }
 
   @Put(':id')
-  updateReport(){
-    return "updated"
+  updateReport(@Body() body:{amount:number,source:string},@Param('type') type:ReportType, @Param('id') id:string){
+    // let reportType = type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    // let response = data.report.filter((data)=>data.type===reportType).find((data)=>data.id===id)
+    
+
+
+    // if(response){
+
+    //   data.report = data.report.map((data)=>{
+    //     if(data.id===id){
+    //       if(amount){
+    //         data.amount=amount
+    //       }
+    //       if(source){
+    //         data.source=source
+    //       }
+    //       data.updatedAt = new Date()
+    //       return data
+    //     }else{
+    //       return data
+    //     }
+    //   })
+
+    //   console.log({data})
+
+    //   return data.report.find((data)=>data.id===id)
+
+      
+    // }else{
+
+    //   return {}
+    // }
+    return this.appService.updateReport(body,type,id)
+    
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport(){
-    return "deleted"
+  deleteReport(@Param('type') type:ReportType , @Param('id') id:string){
+    // let reportType = type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    // let responseIndex = data.report.findIndex((data)=>data.id===id)
+    // console.log({responseIndex})
+
+    // if(responseIndex!=-1){
+    //   data.report.splice(responseIndex,1)
+    //   return  `Deleted Id: ${id}`
+    // }
+
+    return this.appService.deleteReport(type,id)
+    
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,88 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType, data } from './data';
+import {v4 as uuid} from 'uuid'
+
+interface ReportedData{amount:number,source:string}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  getAllIncomeReports(type:ReportType){
+    let reportType = type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    
+    return data.report.filter((data)=>data.type===reportType)
+  }
+
+  getIncomeReportById(type:ReportType,id:string){
+    let reportType = type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    let response = data.report.filter((data)=>data.type===reportType).find((data)=>data.id===id)
+    console.log({response})
+    return response?response:{}
+  }
+
+  createReport(body:ReportedData,type:ReportType){
+    let {source,amount} = body
+    let newReport = {
+      id:uuid(),
+      source:source,
+      amount:amount,
+      createdAt:new Date(),
+      updatedAt:new Date(),
+      type:type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    }
+
+    data.report.push(newReport)
+
+
+    return newReport
+  }
+
+  updateReport(body:ReportedData,type:ReportType,id:string){
+    let {source,amount} = body
+    let reportType = type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    let response = data.report.filter((data)=>data.type===reportType).find((data)=>data.id===id)
+    
+
+
+    if(response){
+
+      data.report = data.report.map((data)=>{
+        if(data.id===id){
+          if(amount){
+            data.amount=amount
+          }
+          if(source){
+            data.source=source
+          }
+          data.updatedAt = new Date()
+          return data
+        }else{
+          return data
+        }
+      })
+
+      console.log({data})
+
+      return data.report.find((data)=>data.id===id)
+
+      
+    }else{
+
+      return {}
+    }
+  }
+
+
+  deleteReport(type:ReportType,id:string){
+    let reportType = type==="income"?ReportType.INCOME:ReportType.EXPENSE
+    let responseIndex = data.report.findIndex((data)=>data.id===id)
+    console.log({responseIndex})
+
+    if(responseIndex!=-1){
+      data.report.splice(responseIndex,1)
+      return  `Deleted Id: ${id}`
+    }
+
+    return `${id} does not exist`
   }
 }
